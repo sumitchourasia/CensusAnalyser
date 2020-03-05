f51e600d31a79909cf22d7892168829c9284922b
sumit <st.sumit01@gmail.com>
2020-03-05 18:35:37 +0530

    [sumit] Refactor . modified code to provide proper documentation

--- CensusAnalyser/CensusAnalyser/Program.cs
+++ CensusAnalyser/CensusAnalyser/Program.cs
@@ -2,12 +2,18 @@
 
 namespace CensusAnalyser
 {
+    /// <summary>
+    /// contains main method
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             string path = @"C:\Users\Bridgelabz\source\repos\CensusAnalyser\CensusAnalyser\CensusAnalyser\FIles\StateCensusData.csv";
-
             Console.WriteLine("Welcome to Census Analyser!");
             StateCensusAnalyser obj = new StateCensusAnalyser();
             Console.WriteLine( obj.LoadstateCensusFile(path));
